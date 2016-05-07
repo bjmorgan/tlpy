877d2ad117f16b5c179b1020c468bfa5933c70f7
Benjamin Morgan <benmorgan2@gmail.com>
2016-05-07 19:56:53 +0100

    Started tidying up, and adding docstrings

--- tlpy/defect.py
+++ tlpy/defect.py
@@ -3,30 +3,62 @@ from tlpy.defect_charge_state import Defect_Charge_State
 import numpy as np
 
 class Defect:
+    """A specific defect corresponding to a specified change in stoichiometry.
+    
+    Attributes:
+        name (str): identifying label for this defect.
+        stoichiometry (dict): the change in stoichiometry associated with forming this defect.
+                              e.g. for an oxygen vacancy this would be { 'O' : -1 }.
+        charge_state (dict): set of Defect_Charge_State objects corresponding to the different charge states for this defect.
+                             the key for each Defect_Charge_State is the defect charge,
+                             e.g. { '-1' : Defect_Charge_State(...) }.
+                             Individual charge states can be added to a Defect object using the add_charge_state method.
+        host (tlpy.host.Host): Host object describing the stoichiometric host system.
+        site (str): identifying label for the defect site in the host structure.""" 
 
     def __init__( self, name, stoichiometry, host, site ):
+        """Create a Defect object."""
         self.name = name
         self.stoichiometry = stoichiometry
-        self.charge_state = {}
         self.host = host
         self.site = site
+        self.charge_state = {}
 
     def add_charge_state( self, charge, energy ):
+        """Create a Defect_Charge_State object, and add it to the self.charge_state dict.
+
+        Args:
+            charge (int): charge for the charge state, e.g. +1 if 1 electron is transferred to the Fermi level.
+            energy (float): energy of this charge state.
+
+        Returns:
+            The new Defect_Charge_State object."""
         self.charge_state[ charge ] = Defect_Charge_State( charge, energy, self.host, self.stoichiometry )
+        return self.charge_state[ charge ]
 
-    def charge_state_energy_at_fermi_energy( self, q, e_fermi, delta_mu ):
-        return self.charge_state[ q ].relative_formation_energy( e_fermi )
-        return self.charge_state[ q ].formation_energy( e_fermi, delta_mu )
+    def charge_state_at_fermi_energy( self, e_fermi ):
+        """Returns the charge state with the lowest formation energy at a given Fermi energy.
 
-    def charge_state_at_fermi_energy( self, e_fermi, delta_mu ):
-        return min( [ ( q, q.formation_energy( e_fermi, delta_mu ) ) for q in self.charge_state.values() ], key = lambda x : x[1] )[0]
+        Args:
+            e_fermi (float): Fermi energy
+
+        Returns:
+            (Defect_Charge_State)"""
+        return min( [ ( q, q.relative_formation_energy( e_fermi ) ) for q in self.charge_state.values() ], key = lambda x : x[1] )[0]
 
     def tl_profile( self, delta_mu, ef_min, ef_max ):
-        # first point is the lowest energy charge state at E_Fermi = 0.0 eV
-        charge_state = self.charge_state_at_fermi_energy( ef_min, delta_mu )
+        """Generates the set of points for a transition level diagram.
+
+        Args:
+            delta_mu (?):
+            ef_min (float):
+            ef_max (float):
+
+        Returns:
+            points (List(tuple)):"""
+        charge_state = self.charge_state_at_fermi_energy( ef_min )
         points = [ ( ef_min, charge_state.formation_energy( ef_min, delta_mu ) ) ]
         q1 = charge_state.charge
-        # where does this line cross other charge state energies?
         while q1 != min( self.charge_state_list() ):
             next_point, next_q = min( ( ( self.transition_level( q1, q2, delta_mu ), q2 ) for q2 in ( q for q in self.charge_state if q < q1 ) ), key = lambda p : p[0][0] )
             if next_point[0] < ef_max:
@@ -38,7 +70,7 @@ class Defect:
         return points
 
     def defect_energy_at_fermi_energy( self, e_fermi, delta_mu ):
-        return self.charge_state_at_fermi_energy( e_fermi, delta_mu ).formation_energy( e_fermi, delta_mu )
+        return self.charge_state_at_fermi_energy( e_fermi ).formation_energy( e_fermi, delta_mu )
 
     def charge_state_list( self ):
         return [ q for q in self.charge_state ]
