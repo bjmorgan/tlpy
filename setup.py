c8b002993757b99cc1f71eb12f64fcaf225df427
Benjamin Morgan <benmorgan2@gmail.com>
2016-05-08 11:07:29 +0100

    Added setup and requirements files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='tlpy',
+      version='0.1',
+      description='Python Transition-Level Utilities',
+      author='Benjamin J. Morgan',
+      author_email='b.j.morgan@bath.ac.uk',
+      packages=['tlpy'],
+     )
