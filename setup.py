9d998ed814c489bb17fc4ee6ad55cfb981680dd6
Benjamin Morgan <benmorgan2@gmail.com>
2017-05-06 08:24:00 +0100

    Added metadata for Travis-CI and Code Climate integration

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
-#!/usr/bin/env python3
+import os
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='tlpy',
-      version='0.1',
-      description='Python Transition-Level Utilities',
-      author='Benjamin J. Morgan',
-      author_email='b.j.morgan@bath.ac.uk',
-      packages=['tlpy'],
-     )
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = '0.1.0'
+
+config = {
+    'description': 'Python Transition-Level Utilities',
+    'long_description': read('README.md'),
+    'author': 'Benjamin J. Morgan',
+    'author_email': 'b.j.morgan@bath.ac.uk',
+    'url': 'https://github.com/bjmorgan/tlpy',
+    'download_url': "https://github.com/bjmorgan/tlpy/archive/%s.tar.gz" % (VERSION),
+    'author_email': 'b.j.morgan@bath.ac.uk',
+    'version': VERSION,
+    'install_requires': [ 'numpy', 
+                          'coverage',
+                          'codeclimate-test-reporter' ],
+    'license': 'MIT',
+    'packages': ['tlpy'],
+    'scripts': [],
+    'name': 'tlpy'
+}
+
+setup(**config)
